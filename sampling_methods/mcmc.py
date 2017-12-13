753255bb308702e21b08392255920885877af2d4
abhinav <jain@cooper.edu>
2017-12-13 16:17:37 -0500

    Fixed MCMC. Need to fix up rejection sampling plots.

--- sampling_methods/mcmc.py
+++ sampling_methods/mcmc.py
@@ -5,11 +5,11 @@ from scipy.stats import multivariate_normal, norm
 import scipy.stats
 
 fig = plt.figure()
-# plt.ylabel("Normalized frequency")
-# plt.xlabel("x")
-# plt.title("EM Algorithm in 1D With 3 Clusters (Iteration %d)" % 0)
-# plt.xlim([-10, 16])
-# plt.ylim([0, 0.7])
+plt.ylabel("w_1")
+plt.xlabel("w_0")
+plt.title("Estimation with MCMC")
+plt.xlim([-0.5, 0.5])
+plt.ylim([-1, 1])
 
 
 # All values from book
@@ -17,10 +17,10 @@ fig = plt.figure()
 N = 25
 sigma = 0.2
 alpha = 2
-w_0 = -0.3
+w_0 = -0.2
 w_1 = 0.5
 
-x_n = np.linspace(-1, 1, N)
+x_n = np.random.uniform(-1, 1, N)
 t_n = w_0 + w_1 * x_n
 noise = np.random.normal(0, sigma, N)
 t_n = t_n + noise
@@ -29,7 +29,7 @@ mu_n = np.zeros((N, 2))
 S_n = np.zeros((N, 2, 2))
 
 mu_0 = [0, 0]
-S_0 = (1.0 / alpha) * (np.identity(2))
+S_0 = sigma * (np.identity(2))
 
 phi = np.array([[1, x_n[0]]])
 for i in range(N):
@@ -37,7 +37,6 @@ for i in range(N):
         phi = np.concatenate((phi, np.array([[1, x_n[i]]])), axis=0)
 phiT = phi.T
 
-plt.scatter(x_n, t_n)
 
 q = mlab.normpdf(x_n, 0, 1)
 
@@ -47,13 +46,11 @@ for zz in range(N):
         k_scale = x_n[zz] / q[zz]
 q = q * k_scale
 
-plt.plot(x_n, q)
-
 z_t = np.random.multivariate_normal(mu_0, S_0)
-
-z_0_accepted = np.zeros(1500)
-num_accepted = 0
 p_z_T = 0
+
+z_0_accepted = np.zeros((1500, 2))
+num_accepted = 0
 
 num_burn = 0
 
@@ -74,14 +71,16 @@ def posterior(t, x, w, noise_sigma, noise_mean, weight_sigma):
 
 
 while num_accepted < 1500:
-    z_star = multivariate_normal(0, 1)
+    z_star = np.random.multivariate_normal(z_t, S_0)
 
-    p_z_T = posterior(t_n, x_n, w_test, sigma, 0, S_0)
+    p_z_T = np.sum(np.log(scipy.stats.multivariate_normal.pdf(t_n, phi[:, 0] * z_t[0] + phi[:, 1] * z_t[1], sigma)))
+    p_z_star = np.sum(np.log(scipy.stats.norm.pdf(t_n, phi[:, 0]*z_star[0] + phi[:, 1]*z_star[1], sigma)))
 
-    p_z_star = posterior(t_n, x_n, w_test, sigma, 0, S_0)
+    p_z_T += np.log(scipy.stats.multivariate_normal.pdf(z_t, mu_0, S_0))
+    p_z_star += np.log(scipy.stats.multivariate_normal.pdf(z_star, mu_0, S_0))
 
     A_z_star = min(0, p_z_star - p_z_T)
-    u = np.random.uniform(0, 1)
+    u = np.log(np.random.uniform(0, 1))
 
     if A_z_star > u:
         num_burn += 1
@@ -89,6 +88,15 @@ while num_accepted < 1500:
         if (num_burn > 100):
             z_0_accepted[num_accepted] = z_star
             num_accepted += 1
-plt.hist(z_0_accepted, bins=100, normed=True)
+w_0_mean = np.sum(z_0_accepted[:, 0])/num_accepted
+w_1_mean = np.sum(z_0_accepted[:, 1])/num_accepted
+print("Actual values: w_0 = ", w_0, ", w_1 = ", w_1)
+print("Estimated values: w_0 = ", w_0_mean, ", w_1 = ", w_1_mean)
+
+plt.scatter(z_0_accepted[:, 0], z_0_accepted[:, 1], c="blue", alpha=0.2, label="Sampled values")
+plt.scatter(w_0, w_1, c="green", marker="x", label="Actual weights")
+plt.scatter(w_0_mean, w_1_mean, c="red", marker="x", label="Estimated weights")
+plt.legend(loc="upper right")
+
 
 plt.show()
